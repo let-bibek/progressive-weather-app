180067a491196cd50c6793d18217002bd7b171bc
let_bibek <kumarbibek094@gmail.com>
2023-03-05 00:17:19 +0545

    initials

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
+import fetchWeather from './api/fetchWeather';
+import { FaAirbnb } from "react-icons/fa";
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ const App=()=>{
+    const [city,setCity]=useState('');
+    const [weather, setWeather] = useState({});
+    const search=async(e)=>{
+            if(e.key=="Enter"){
+                const weatherData=await fetchWeather(city);
+                setWeather(weatherData);
+            }
+    }
+    return (
+      <div className="main-container">
+        <input
+          type="text"
+          className="search"
+          placeholder="Search weather..."
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+          onKeyUp={search}
+        />
+        {
+            weather.main&&(
+                <div className='city'>
+                    <h2 className='city-name'>
+                        <span>{weather.name}</span>
+                        <sup>{weather.sys.country}</sup>
+                    </h2>
+                </div>
+            )
+        }
+      </div>
+    );
 }
 
 export default App;
