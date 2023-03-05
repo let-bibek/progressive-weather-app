213316259adc971321eeae2c4c85dbd839864c07
let_bibek <kumarbibek094@gmail.com>
2023-03-05 20:53:28 +0545

    frontend complete

--- src/App.js
+++ src/App.js
@@ -1,38 +1,82 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import fetchWeather from './api/fetchWeather';
 import { FaAirbnb } from "react-icons/fa";
 import './App.css';
- const App=()=>{
-    const [city,setCity]=useState('');
-    const [weather, setWeather] = useState({});
-    const search=async(e)=>{
-            if(e.key=="Enter"){
-                const weatherData=await fetchWeather(city);
-                setWeather(weatherData);
-            }
+const App = () => {
+  const [city, setCity] = useState('');
+  const [weather, setWeather] = useState({});
+  const [sunrise, setSunrise] = useState('20');
+ 
+  const search = async (e) => {
+    if (e.key == "Enter") {
+      const weatherData = await fetchWeather(city);
+      setWeather(weatherData);
+      setCity('');
     }
-    return (
-      <div className="main-container">
-        <input
-          type="text"
-          className="search"
-          placeholder="Search weather..."
-          value={city}
-          onChange={(e) => setCity(e.target.value)}
-          onKeyUp={search}
-        />
-        {
-            weather.main&&(
-                <div className='city'>
-                    <h2 className='city-name'>
-                        <span>{weather.name}</span>
-                        <sup>{weather.sys.country}</sup>
-                    </h2>
+  }
+
+
+  return (
+    <div className="main-container">
+      <input
+        type="text"
+        className="search"
+        placeholder="Search weather..."
+        value={city}
+        onChange={(e) => setCity(e.target.value)}
+        onKeyUp={search}
+      />
+      {weather.main && (
+        <>
+          <div className="city">
+            <h2 className="city-name">
+              <span>{weather.name}</span>
+              <sup>{weather.sys.country}</sup>
+            </h2>
+            <div className="city-temp">
+              {Math.round(weather.main.temp)}
+              <sup>&deg;</sup>C
+            </div>
+            <div className="info">
+              <img
+                className="city-icon"
+                src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+                alt={weather.weather[0].description}
+              />
+              <p>{weather.weather[0].description}</p>
+            </div>
+            <div className="additional-info">
+              <h3>Additional info</h3>
+              <div className="two-col">
+                <div>Min/Max</div>
+                <div>
+                  {Math.round(weather.main.temp_min) +
+                    "/" +
+                    Math.round(weather.main.temp_max)}
+                  <sup>&deg;</sup>C
                 </div>
-            )
-        }
-      </div>
-    );
+              </div>
+              <br />
+              <div className="two-col">
+                <div>Humidity</div>
+                <div>{Math.round(weather.main.humidity)}%</div>
+              </div>
+              <br />
+              <div className="two-col">
+                <div>Wind</div>
+                <div>{weather.wind.speed} mph</div>
+              </div>
+              <br />
+              <div className="two-col">
+                <div>Visibility</div>
+                <div>{weather.visibility}mi</div>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default App;
--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -2,14 +2,14 @@ import axios from 'axios';
 const URL = "https://api.openweathermap.org/data/2.5/weather";
 const API_KEY = '09af124a27b2d94afa413b3729579669';
 const fetchWeather=async(query)=>{
-    const {weather_data}=await await axios.get(URL,{
+    const {data}=await await axios.get(URL,{
         params:{
             q:query,
             units:'metric',
-            appid:API_KEY
+            appid:API_KEY,
         }
     });
-    return weather_data;
+    return data;
 }
 
 export default fetchWeather;
